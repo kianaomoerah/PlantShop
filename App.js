979f8e7ad6084ec30a3c10ae5905178002640ee3
Kiana Omoerah <kiana.omoerah@gmail.com>
2023-04-25 23:39:50 -0400

    add safe area view component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <Text>Welcome to the plant shop!</Text>
-      <HomeScreen />
+      <SafeAreaView>
+        <Text>Welcome to the plant shop!</Text>
+        <HomeScreen />
+      </SafeAreaView>
     </>
 
   );
