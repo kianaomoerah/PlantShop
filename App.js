6e35e1521f259438e7a9a6559d2abe2a4ca0edc8
Kiana Omoerah <kiana.omoerah@gmail.com>
2023-05-01 21:53:04 -0400

    add bottoms tab button to show home shop screen and checkout screen

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import IconButton from './components/IconButton';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import CheckoutScreen from './screens/CheckoutScreen';
 import PlantDetailScreen from './screens/PlantDetailScreen';
+import COLORS from './constants/colors';
 
 const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function AppOverview() {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarColor: COLORS.white,
+        tabBarStyle: { backgroundColor: COLORS.darkGreen},
+        tabBarActiveTintColor: COLORS.white,
+        tabBarInactiveTintColor: COLORS.lightGreen
+      }}
+    >
+      <BottomTabs.Screen 
+      name="Shop"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: ({color, size}) => <Ionicons name="leaf" size={36} color={COLORS.white}
+     />
+      }}
+      />
+      <BottomTabs.Screen 
+      name="Cart"
+      component={CheckoutScreen}
+      options={{
+        tabBarIcon: ({color, size}) => <Ionicons name="cart" size={36} color={COLORS.white} />
+      }}
+      />
+    </BottomTabs.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -13,8 +49,8 @@ export default function App() {
     <StatusBar style="auto" />
     <Stack.Navigator>
       <Stack.Screen 
-      name="Home" 
-      component={HomeScreen}
+      name="AppOverview" 
+      component={AppOverview}
       options={{
         headerShown: false,
         contentStyle: { backgroundColor: '#fff'}
@@ -31,14 +67,3 @@ export default function App() {
   </NavigationContainer>
   );
 }
-
-// todo: drawer navigator to show home screen and cart ??
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
---
+++ screens/CheckoutScreen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native';
+
+function CheckoutScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Check out screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default CheckoutScreen;
