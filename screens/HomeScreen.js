14e29c8897ed99852fa0e1f983eb34efc027c8ec
Kiana Omoerah <kiana.omoerah@gmail.com>
2023-05-03 23:24:08 -0400

    update home screen background color

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,21 @@
-import { SafeAreaView, Text, ScrollView } from "react-native";
-import PlantList from "../components/PlantList";
+import { SafeAreaView, StyleSheet } from 'react-native';
+import PlantList from '../components/PlantList';
+import COLORS from '../constants/colors';
 
 function HomeScreen() {
   return (
-    <SafeAreaView>
-      {/* header with shopping cart icon */}
+    
+    <SafeAreaView style={styles.homeScreen}>
       <PlantList />
     </SafeAreaView>
- 
-  
 
   )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  homeScreen: {
+    backgroundColor: COLORS.white
+  }
+})
