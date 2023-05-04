51b65edc4df40d9531febd5064f91f98e31b9591
Kiana Omoerah <kiana.omoerah@gmail.com>
2023-05-03 23:18:06 -0400

    complete css, render of cart items and connect to cart context

--- components/Cart.js
+++ components/Cart.js
@@ -1,141 +1,146 @@
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Image, StyleSheet, SafeAreaView} from 'react-native';
 import Button from '../components/Button';
 import { CartContext } from '../store/cart-context';
 import { useContext } from 'react';
+import COLORS from '../constants/colors'
+import IconButton from './IconButton';
 
 function Cart() {
 
   const cart = useContext(CartContext);
-
-  // const cartItems = PLANTS.filter( plant => cart.items.includes(plant.id));
-
   const cartItems = cart.items 
-  // console.log(cartItems)
-  // console.log(`${cart.items.id}`)
-
-  // const cartItemIds = cartItems.map((item) => {
-  //   console.log(`${item.id} ${item.quantity} ${item.name} ${item.price}`)
-  //   console.log(`${item}`)
-  // })
-
-  // const cartItemData = cartItems.map((plant) => {
-
-  // })
-
-  // const plantData = (id) => {
-  //   return plantObject = PLANTS.find( plant => plant.id === id)
-  // }
 
   function renderCartItem(itemData) {
 
     return (
-      <View style={styles.item}>
-        {/* <Image source={itemData.item.image} /> */}
-        <Text>{itemData.item.name}</Text>
-        <Text>{itemData.item.quantity}</Text>
-        <Text>$ {itemData.item.price}.00</Text>
+      <View style={styles.cartItemContainer}>
+        {/* todo: clean up props? */}
+        <View style={styles.imageContainer}>
+          <Image source={itemData.item.image} style={styles.image}/>
+        </View>
+        <View style={styles.nameContainer}>
+          <Text>{itemData.item.name}</Text>
+        </View>
+        <View style={styles.quantityContainer}>
+          <IconButton 
+            onPress={() => cart.removeItem(itemData.item.id, itemData.item.name, itemData.item.image, itemData.item.price)}
+            icon={'remove-circle'}
+            color={COLORS.lightGreen}
+          />
+            <Text>{itemData.item.quantity}</Text>
+          <IconButton 
+            onPress={() => cart.addItem(itemData.item.id, itemData.item.name, itemData.item.image, itemData.item.price)}
+            icon={'add-circle'}
+            color={COLORS.lightGreen}
+          />
+        </View>
+        <View>
+          <Text>$ { itemData.item.quantity * itemData.item.price}.00</Text>
+        </View>
       </View>
-      // wrap in views to create columns
     )
   }
 
 
   if (cartItems.length === 0) {
     return (
-      <View>
-        <Text>Your cart is empty, add a new plant friend!</Text>
+      <View style={styles.emptyCart}>
+        <Text style={[styles.title, styles.emptyCartText]}>Your Cart is Empty, {'\n'}Add a New Plant Friend!</Text>
       </View>
     )
   }
 
   return (
-    <View>
-      <Text>Your cart</Text>
-
-      <View>
-        <FlatList 
-          data={cartItems}
-          keyExtractor={(cartItem) => cartItem.id}
-          renderItem={renderCartItem}
-        />
-      </View>
-    </View>
+    <SafeAreaView style={styles.view}>
+      <FlatList 
+        data={cartItems}
+        keyExtractor={(cartItem) => cartItem.id}
+        renderItem={renderCartItem}
+        contentContainerStyle={
+          {
+            backgroundColor: COLORS.white,
+            // height: '100%',
+            justifyContent: 'space-between',
+            // marginVertical: 25
+          }
+        }
+        ListHeaderComponent={
+          <Text style={styles.title}>Your cart:</Text>
+        }
+        ListFooterComponent={
+          <View style={styles.totalContainer}>
+            <Text style={styles.totalText}>Total: $ {cart.getTotal()}</Text>
+            <Button 
+            style={styles.button}
+            // todo: is this the right design choice?
+            primaryColor={COLORS.lightBeige} 
+            secondaryColor={COLORS.lightGreen}
+            alignment={'center'}>Complete Order</Button>
+          </View>
+        }
+      />
+    </SafeAreaView>
   )
-
-  // return (
-  //   <View>
-  //     <Text>Items in your cart: </Text>
-  //     {
-  //       cart.items.map((currentProduct, id) => (
-  //         <Text>{currentProduct.id}</Text>
-  //       ))
-  //     }
-  //     <Text>Total: </Text>
-
-  //     <Button>Purchase Items</Button>
-  //   </View>
-
-
-  // )
-
-  // function renderCartItem(itemData) {
-  //   const cartItem = itemData.item
-  //   const cartItemProps = {
-  //     id: cartItem.id,
-  //     quantity: cartItem.quantity,
-  //     name: cartItem.name,
-  //     price: cartItem.price
-  //   }
-  
-
-  // return (
-  //   <View>
-  //     {/* <Image source={image}/> */}
-  //     <Text>{itemData.cartItem.name}</Text>
-  //     <Text>{itemData.cartItem.name}</Text>
-  //     {/* <Text>{}</Text> */}
-  //   </View>
-  // )
-  // }
-
-  // return (
-  //   // cartItems.map(( cartItem ) => {
-
-  //   //   // const plantItem = plantData(plant.id)
-
-  //   //   // console.log(`${plantItem.title}`)
-
-  //   //   // const plantObject = PLANTS.find( cartItem => cartItem.id === id)
-
-  //   //   // console.log(`${plantObject}`)
-
-  //   //   console.log(`${cartItem} $$`)
-
-  //   // })
-  //   // <Text>{cartItems}</Text>
-
-  //   <>
-  //     <Text>hello</Text>
-  //     <FlatList 
-  //       data={cartItems}
-  //       keyExtractor={(cartItem) => cartItem.id}
-  //       renderItem={renderCartItem}
-  //     />
-  //   </>
-
-  // )
-
-  // map through my cart items array
-  // using the id in this context, find the plant object in the plant data 
-  // display the plant information and the quantity 
-  
 }
 
 export default Cart;
 
 const styles = StyleSheet.create({
-  item: {
+  view: {
+    flex: 1,
+    backgroundColor: COLORS.white
+  },  
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 26,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  emptyCart: {
+    justifyContent: 'center',
+    backgroundColor: COLORS.white,
+    height: '100%'
+  },
+  emptyCartText: {
+    lineHeight: 30,
+    fontSize: 20
+  },
+  flatList: {
+    marginBottom: 100
+  },
+  cartItemContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 15,
+    marginVertical: 15
+  },
+  imageContainer: {
+    backgroundColor: COLORS.lightBeige,
+    borderRadius: 10
+  },
+  image: {
+    width: 75,
+    height: 75,
+    resizeMode: 'contain'
+  },
+  nameContainer: {
+    flex: 0.3
+  },
+  quantityContainer: {
+    flex: 0.35,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: 80
+  },
+  totalContainer: {
+    marginVertical: 7,
+    marginHorizontal: 15
+  },
+  totalText: {
+    fontWeight: 'bold',
+    fontSize: 18
   }
 })
